db258d4be1fd3aef19101f4bfc2d4198bfc5b19c
anascode23 <anas.ali@ecutbildning.se>
2024-02-20 14:46:36 +0000

    Add project files.

---
+++ BulkyWeb/Data/BulkyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class BulkyDbContext : DbContext
+    {
+        public BulkyDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        DbSet<Category> categories { get; set; }
+    }
+}
